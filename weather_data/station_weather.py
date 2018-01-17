d12e316c917b50089b4de104bdef8f0dfcb91e04
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-01-17 11:38:06 +0100

    fetching data for longer periods + logging missing values

--- weather_data/station_weather.py
+++ weather_data/station_weather.py
@@ -1,5 +1,6 @@
 import datetime
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, HTTPError
+from dateutil import rrule
 from weather_data.stations import get_station
 import os
 
@@ -7,7 +8,6 @@ import os
 class Station:
     """
     Meteo station to get the weather conditions results for   
-    
     """
     def __init__(self, station_name):
         """
@@ -24,9 +24,22 @@ class Station:
         """
         return list(get_station(self.name)['station_id'])[0]
 
-    def fetch_weather_conditions(self, year, month, save_data=True):
+    def fetch_data_for_period(self, start_date, end_date):
+        missing_periods = []
+        for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
+            first_day = dt.replace(day=1)
+            month = datetime.datetime.strftime(first_day, "%m")
+            year = datetime.datetime.strftime(first_day, "%Y")
+            try:
+                self.fetch_weather_conditions(int(year), int(month))
+            except HTTPError:
+                print("WARNING: Missing data for {} station in {}/{}".format(self.name, month, year))
+                missing_periods.append("{}/{}".format(year, month))
+        return "Missing periods for {} station: ".format(self.name) + ", ".join(missing_periods)
+
+    def fetch_weather_conditions(self, year, month):
         """
-        Download station's weather conditions from imgw server for specific month and year.
+        Download station's weather conditions from IMGW server for specific month and year.
         Save it to local directory 
         :param year: year to retrieve results for
         :type year: int
@@ -34,7 +47,7 @@ class Station:
         :type month: int
         :param save_data: save the results in local directory?
         :type save_data: bool
-        :return: url of retrieved data
+        :return: relative path to local dir where the file has been saved to 
         :rtype  str
         """
         date = datetime.date(year, month, 1)
@@ -42,11 +55,10 @@ class Station:
         url_prefix = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/"
         sub_dir = "{}/{}/".format(year, month)
         filename = "dane_{}_{}_{}.csv.gz".format(year, month, self.id)
-        local_dir = "./input_data/imgw/data/station_{}_{}/".format(self.id, self.name)
+        local_dir = "./input_data/imgw/data/station_{}_{}/{}/".format(self.id, self.name, year)
         local_filename = "{}_{}_{}.csv.gz".format(self.id, year, month)
         if not os.path.exists(local_dir):
             os.makedirs(local_dir)
         url = url_prefix + sub_dir + filename
-        if save_data:
-            urlretrieve(url, local_dir + local_filename)
+        urlretrieve(url, local_dir + local_filename)
         return local_dir + local_filename
